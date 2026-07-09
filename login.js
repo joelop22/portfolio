212e0ad7452de54b0cf8accb17b744a65fa60036
joelop22 <95672411+joelop22@users.noreply.github.com>
2026-07-09 14:36:48 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,30 @@
-document.getElementById('login-form').addEventListener('submit', (e) => {
+document.getElementById('login-form').addEventListener('submit', async (e) => {
     e.preventDefault();
+
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
     const errorMsg = document.getElementById('error-msg');
 
     if (user === 'joe.el' && pass === '53732004') {
-        sessionStorage.setItem('auth', 'true');
-        window.location.href = 'admin.html';
+
+        try {
+
+            await firebase.auth().signInWithEmailAndPassword(
+                "joelshajan2004@gmail.com",
+                "53732004"
+            );
+
+            sessionStorage.setItem('auth', 'true');
+            window.location.href = 'admin.html';
+
+        } catch (error) {
+            console.error(error);
+            errorMsg.innerText = "Firebase login failed.";
+            errorMsg.style.display = 'block';
+        }
+
     } else {
+        errorMsg.innerText = "Invalid credentials.";
         errorMsg.style.display = 'block';
     }
 });
